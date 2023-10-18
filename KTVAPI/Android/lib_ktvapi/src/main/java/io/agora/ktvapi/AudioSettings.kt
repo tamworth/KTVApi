cdaad5d2b079b50dc0d4ce81a5350109320762a5
CYG <cyg@CYGdeMacBook-Pro-2.local>
2023-10-18 16:19:00 +0800

    add readme

--- KTVAPI/Android/lib_ktvapi/src/main/java/io/agora/ktvapi/AudioSettings.kt
+++ KTVAPI/Android/lib_ktvapi/src/main/java/io/agora/ktvapi/AudioSettings.kt
@@ -7,6 +7,16 @@ enum class SceneType(val value: Int) {
     ChatRoom(1)
 }
 
+/**
+ * 虚拟声卡类型
+ * @param NanShengCiXing 男生磁性
+ * @param NvShengCiXing 女生磁性
+ * @param NanShengTianMei 男生甜美 延迟大(60ms) 男声甜美在k歌场景下会被映射到男生唱歌
+ * @param NvShengTianMei 女生甜美 延迟大(60ms) 女生甜美在k歌场景下会被映射到女生唱歌
+ * @param NanShengChangGe 男生唱歌
+ * @param NvShengChangGe 女生唱歌
+ * @param Close 关
+ */
 enum class SoundCardType constructor(
     val gainValue: Float,
     val presetValue: Int,
@@ -22,30 +32,60 @@ enum class SoundCardType constructor(
     Close(gainValue = -1.0f, presetValue = -1, gender = -1, effect = -1)
 }
 
+/**
+ * 插入耳机的类型
+ * @param EQ0 针对小米系列有线耳机
+ * @param EQ1 针对Sony系列有线耳机
+ * @param EQ2 针对JBL系列有线耳机
+ * @param EQ3 针对华为系列有线耳机
+ * @param EQ4 针对iphone系列有线耳机(默认)
+ */
+enum class EarPhoneType constructor(val presetValue: Int) {
+    EQ0(0),
+    EQ1(1),
+    EQ2(2),
+    EQ3(3),
+    EQ4(4),
+}
+
 class AudioSettings constructor(private val mRtcEngine: RtcEngine) {
     companion object {
         var enabled: Boolean = false
     }
 
-    fun enableVirtualSoundCard(sceneType: SceneType, soundCardType: SoundCardType) {
+    fun enableVirtualSoundCard(sceneType: SceneType, soundCardType: SoundCardType, earPhoneType: EarPhoneType?) {
         enabled = soundCardType != SoundCardType.Close
-        setVirtualSoundCardType(sceneType, soundCardType)
+        setVirtualSoundCardType(sceneType, soundCardType, earPhoneType)
         if (sceneType == SceneType.KTV) {
             mRtcEngine.setParameters("{\"che.audio.agc.enable\": ${!enabled}}")
         }
     }
 
-    private fun setVirtualSoundCardType(sceneType: SceneType, soundCardType: SoundCardType) {
+    fun enableAGC(enable: Boolean) {
+        mRtcEngine.setParameters("{\"che.audio.agc.enable\": $enable}")
+    }
+
+    private fun setVirtualSoundCardType(sceneType: SceneType, soundCardType: SoundCardType, earPhoneType: EarPhoneType?) {
         // 延迟大(60ms)建议男声甜美在k歌场景下 映射到唱歌私参
         if (sceneType == SceneType.KTV && soundCardType == SoundCardType.NanShengTianMei) {
-            setVirtualSoundCardType(SceneType.ChatRoom, SoundCardType.NanShengChangGe)
+            setVirtualSoundCardType(SceneType.ChatRoom, SoundCardType.NanShengChangGe, earPhoneType)
             return
         }
         // 延迟大(60ms)建议女生甜美在k歌场景下 映射到唱歌私参
         if (sceneType == SceneType.KTV && soundCardType == SoundCardType.NvShengTianMei) {
-            setVirtualSoundCardType(SceneType.ChatRoom, SoundCardType.NvShengChangGe)
+            setVirtualSoundCardType(SceneType.ChatRoom, SoundCardType.NvShengChangGe, earPhoneType)
             return
         }
+
+        // 有预设的耳机类型
+        earPhoneType?.let {
+            if (soundCardType != SoundCardType.Close) {
+                mRtcEngine.setParameters("{\"che.audio.virtual_soundcard\":{\"preset\":${earPhoneType.presetValue},\"gain\":${soundCardType.gainValue},\"gender\":${soundCardType.gender},\"effect\":${soundCardType.effect}}}")
+                return
+            }
+        }
+
+        // 默认耳机类型
         mRtcEngine.setParameters("{\"che.audio.virtual_soundcard\":{\"preset\":${soundCardType.presetValue},\"gain\":${soundCardType.gainValue},\"gender\":${soundCardType.gender},\"effect\":${soundCardType.effect}}}")
     }
 }
